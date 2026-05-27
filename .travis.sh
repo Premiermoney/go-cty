e0ff6a8ccb422b8ba39590b1699b722800d0eb99
Premiermoney <286675216+Premiermoney@users.noreply.github.com>
2026-05-27 15:53:01 -0400

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-set -e
+
+
+
+
+set -r
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v vendor); do
-    go test -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
-done
